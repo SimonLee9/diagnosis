2c676e8b9519bd8b6d709d2d72658842dcfc1a2f
SimonLee9 <lbj142632@gmail.com>
2025-02-24 18:11:50 +0900

    update - shortcut

---
+++ slamnav2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/rainbow/rplidar_sdk/output/Linux/Release
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/rainbow/OrbbecSDK/SDK/lib
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/rainbow/slamnav2
+
+cd /home/rainbow/slamnav2 && ./SLAMNAV2
+
